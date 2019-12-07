2951cc355fc1af776b4c6056f42d88e2ab18f5ee
Ken Hibino <ken.hibino7@gmail.com>
2019-12-07 09:15:46 -0800

    Remove cobra comments

--- tools/asynqmon/cmd/root.go
+++ tools/asynqmon/cmd/root.go
@@ -42,10 +42,6 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.asynqmon.yaml)")
 	rootCmd.PersistentFlags().StringVarP(&uri, "uri", "u", "127.0.0.1:6379", "Redis server URI")
 	rootCmd.PersistentFlags().IntVarP(&db, "db", "n", 0, "Redis database number (default is 0)")
