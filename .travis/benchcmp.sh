9b7b1bbff6b930cb11f16eadaa22ff272ef1c3fd
Ken Hibino <ken.hibino7@gmail.com>
2020-06-06 10:38:54 -0700

    Update benchmark.sh to print debug logs

--- .travis/benchcmp.sh
+++ .travis/benchcmp.sh
@@ -4,9 +4,11 @@ if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ]; then
     git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
     cd "${TRAVIS_REPO_SLUG}-bench" && \
     # Benchmark master
+    echo "Running benchmark tests on master branch"
     git checkout master && \
     go test -run=XXX -bench=. -loglevel=debug ./... > master.txt && \
     # Benchmark feature branch
+    echo "Running benchmark tests on feature branch"
     git checkout ${TRAVIS_COMMIT} && \
     go test -run=XXX -bench=. -loglevel=debug ./... > feature.txt && \
     go get -u golang.org/x/tools/cmd/benchcmp && \
