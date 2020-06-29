7b1542fbef526a308d9b69469d52a9f38e84db3e
Ken Hibino <ken.hibino7@gmail.com>
2020-06-29 13:56:53 -0700

    Add version command to CLI

--- internal/base/base.go
+++ internal/base/base.go
@@ -17,6 +17,9 @@ import (
 	"github.com/rs/xid"
 )
 
+// Version of asynq library and CLI.
+const Version = "0.10.0"
+
 // DefaultQueueName is the queue name used if none are specified by user.
 const DefaultQueueName = "default"
 
