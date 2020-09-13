82b6828f434904428225b90e9ff404bb133361e4
Ken Hibino <ken.hibino7@gmail.com>
2020-09-13 06:03:11 -0700

    Replace benchcmp with benchstat

--- .travis/benchstat.sh
+++ .travis/benchstat.sh
@@ -2,17 +2,19 @@ if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ]; then
     REMOTE_URL="$(git config --get remote.origin.url)";
     cd ${TRAVIS_BUILD_DIR}/.. && \
     git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
+    # turn the detached message off
+    git config --global advice.detachedHead false && \
     cd "${TRAVIS_REPO_SLUG}-bench" && \
 
     # Benchmark master
     git checkout master && \
-    go test -run=XXX -bench=. ./... > master.txt && \
+    go test -run=^$ -bench=. -count=5 -timeout=60m -benchmem ./... > master.txt && \
 
     # Benchmark feature branch
     git checkout ${TRAVIS_COMMIT} && \
-    go test -run=XXX -bench=. ./... > feature.txt && \
+    go test -run=^$ -bench=. -count=5 -timeout=60m -benchmem ./... > feature.txt && \
 
     # compare two benchmarks
-    go get -u golang.org/x/tools/cmd/benchcmp && \
-    benchcmp master.txt feature.txt;
+    go get -u golang.org/x/perf/cmd/benchstat && \
+    benchstat master.txt feature.txt;
 fi
