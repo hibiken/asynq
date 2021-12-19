5f20edcbd10b92950aa2a1d2b69e9bf8316a642a
Ken Hibino <ken.hibino7@gmail.com>
2021-12-19 07:00:21 -0800

    v0.20.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.20.0] - 2021-12-19
+
 ### Added
 
 - Package `x/metrics` is added.
--- internal/base/base.go
+++ internal/base/base.go
@@ -22,7 +22,7 @@ import (
 )
 
 // Version of asynq library and CLI.
-const Version = "0.19.1"
+const Version = "0.20.0"
 
 // DefaultQueueName is the queue name used if none are specified by user.
 const DefaultQueueName = "default"
