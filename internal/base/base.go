75f6504aaf50a1a95fcdff8020a2951564dc12c3
aradwann <aradwann@proton.me>
2024-10-27 19:12:29 +0300

    fmt

--- internal/base/base.go
+++ internal/base/base.go
@@ -406,7 +406,6 @@ func DecodeServerInfo(b []byte) (*ServerInfo, error) {
 	}
 	startTime := pbmsg.GetStartTime()
 
-
 	return &ServerInfo{
 		Host:              pbmsg.GetHost(),
 		PID:               int(pbmsg.GetPid()),
@@ -508,7 +507,6 @@ func EncodeSchedulerEntry(entry *SchedulerEntry) ([]byte, error) {
 	}
 	next := timestamppb.New(entry.Next)
 	prev := timestamppb.New(entry.Prev)
-
 
 	return proto.Marshal(&pb.SchedulerEntry{
 		Id:              entry.ID,
